d77b7a2145de25cee3c1910e857805fb63f9cf4b
Alejandro Gonzalvez <alejandrogonzalvez@Alejandros-MacBook-Pro.local>
2021-05-26 21:07:46 -0400

    Relinked the script.js file. Momentjs issue fixed.

--- script.js
+++ script.js
@@ -2,7 +2,5 @@ var timeDisplay = $('#currentDay');
 var saveButton = document.querySelector('.saveBtn');
 
 
-currentDay = setInterval(function () {
-    $('#currentDay').text(moment().format('MMMM Do YYYY, h:mm:ss A, dddd'));
-}, 1000);
-  
+var today= moment();
+$(timeDisplay).text(today.format("MMM Do, YYYY"));
