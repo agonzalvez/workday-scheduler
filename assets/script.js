7103a0c30a932eff1e4bf521b065aec950357c36
Alejandro Gonzalvez <alejandrogonzalvez@Alejandros-MacBook-Pro.local>
2021-05-26 23:05:12 -0400

    minor updates/comments added to js file.

--- assets/script.js
+++ assets/script.js
@@ -2,6 +2,7 @@ var timeDisplay = $('#currentDay');
 var saveButton = document.querySelector('.saveBtn');
 
 
+
 // Current day and date using moment.js
 var today = moment().format('dddd, MMMM Do YYYY');
 timeDisplay.text(today);
@@ -10,7 +11,15 @@ timeDisplay.text(today);
 
 saveButton.addEventListener("click", savedActivity);
 
+// Local storage
 function savedActivity() {
     var activityValue = document.querySelector('.activity').value;
     localStorage.setItem("activityValue", activityValue);
 }
+
+// function getActivity(){
+//     var activityValue = document.querySelector('.activity').value;
+//     document.getElementsByClassName(".activity").innerHTML = localStorage.getItem(activityValue);
+// }
+
+// getActivity();
