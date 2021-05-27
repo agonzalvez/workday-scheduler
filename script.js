6548f514f58929494a1ca61f91d0a04cb1204125
Alejandro Gonzalvez <alejandrogonzalvez@Alejandros-MacBook-Pro.local>
2021-05-26 21:30:42 -0400

    Created local storage. It works for the first field but not the rest.

--- script.js
+++ script.js
@@ -2,5 +2,17 @@ var timeDisplay = $('#currentDay');
 var saveButton = document.querySelector('.saveBtn');
 
 
-var today= moment();
-$(timeDisplay).text(today.format("MMM Do, YYYY"));
+function currentDay(){
+
+    var today=  moment().format('dddd, MMMM Do YYYY');
+     timeDisplay.text(today);
+ }
+ setInterval(currentDay, 1000);
+
+
+saveButton.addEventListener("click", savedActivity);
+
+function savedActivity (){
+    var activityValue = document.querySelector('.activity').value;
+    localStorage.setItem("activityValue", activityValue );
+}
