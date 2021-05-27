81f573be369c32bc97ee31d21a46fb075b4f9ac0
Alejandro Gonzalvez <alejandrogonzalvez@Alejandros-MacBook-Pro.local>
2021-05-27 10:51:57 -0400

    Attempted to add change background color functions to match the time of the day, i.e. past, present, future. Fixes pending

--- assets/script.js
+++ assets/script.js
@@ -8,18 +8,61 @@ var today = moment().format('dddd, MMMM Do YYYY');
 timeDisplay.text(today);
 
 
+saveButton.addEventListener("click", storeActivity);
 
-saveButton.addEventListener("click", savedActivity);
+
+function storeActivity (){
+    var activityValue = document.querySelector('.activity').value;
+    var description = $(this).siblings(".activity").val()
+    console.log(description)
+    var time = $(this).parent().attr("id")
+    console.log(time)
+    localStorage.setItem(time, description);
+    
+}
+
+$("#09 .activity").val(localStorage.getItem("09"))
+$("#10 .activity").val(localStorage.getItem("10"))
+$("#11 .activity").val(localStorage.getItem("11"))
+$("#12 .activity").val(localStorage.getItem("12"))
+$("#13 .activity").val(localStorage.getItem("13"))
+$("#14 .activity").val(localStorage.getItem("14"))
+$("#15 .activity").val(localStorage.getItem("15"))
+$("#16 .activity").val(localStorage.getItem("16"))
+$("#17 .activity").val(localStorage.getItem("17"))
+
+
+function hourBlock(){
+    var now = moment().hours();
+ 
+    $(".row").each(function(){
+       var blockTime = parseInt($(this).attr("id"))
+ 
+       if (now > blockTime){
+          $(this).addClass("past")
+       } else if (now === blockTime){
+          $(this).removeClass("past")
+          $(this).addClass("present")
+       } else {
+       $(this).removeClass("past")
+       $(this).removeClass("present")
+       $(this).addClass("future")
+       }
+    })
+ 
+ }
+ hourBlock();
 
 // Local storage
-function savedActivity() {
-    var activityValue = document.querySelector('.activity').value;
-    localStorage.setItem("activityValue", activityValue);
-}
+// function savedActivity() {
+//     var activityValue = document.querySelector('.activity').value;
+//     localStorage.setItem("activityValue", activityValue);
+// }
 
 // function getActivity(){
 //     var activityValue = document.querySelector('.activity').value;
 //     document.getElementsByClassName(".activity").innerHTML = localStorage.getItem(activityValue);
 // }
 
-// getActivity();
+// getActivity();\
+
